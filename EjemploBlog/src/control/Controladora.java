6f0043857371366ff898de0c0d5f3b4f6599dd63
Indigo Sánchez <indigonikkis@gmail.com>
2026-04-30 01:38:59 -0600

    Add files via upload

--- EjemploBlog/src/control/Controladora.java
+++ EjemploBlog/src/control/Controladora.java
@@ -10,16 +10,20 @@ public class Controladora {
 	public Controladora() {
 		blogs = new TreeMap<Integer, Blog>();
 	}
+	
+	//BLOG
+	
 	public void crearBlog(String nombre, String descripcion) {
 		Blog b = new Blog(nombre, descripcion);
 		blogs.put(b.getCodigo(), b);	//añade código con su blog
 	}
+	
 	public void borrarBlog(int codigoBlog) throws Exception {
-		if (!blogs.containsKey(codigoBlog)) 
-			throw new Exception("El código de blog no es válido");
+		revisarBlogExistente(codigoBlog);
 		blogs.remove(codigoBlog);
 	}
 	
+	//lista con código y nombre de blogs
 	public Map<Integer, String> obtenerBlogs() {
 		Map<Integer, String> resultado = new TreeMap<Integer, String>();
 		for (Blog b : blogs.values()) {
@@ -27,12 +31,38 @@ public class Controladora {
 		}
 		return resultado;
 	}
+	
+	//PUBLICACIÓN
+	
 	public void crearPublicacion(int codigoBlog, String titulo, String texto, String autor) throws Exception {
-		if (!blogs.containsKey(codigoBlog)) 
-			throw new Exception("El código de blog no es válido");
+		
+		revisarBlogExistente(codigoBlog);
 		Blog b = blogs.get(codigoBlog);
 		b.crearPublicacion(titulo, texto, autor);
 	}
 	
+	// mapa con código y titulo de cada publicacion
+	public Map<Integer,String> obtenerPublicaciones(int codigoBlog) throws Exception {
+		revisarBlogExistente(codigoBlog);
+		Blog b = blogs.get(codigoBlog);
+		return b.obtenerTitulosPublicaciones();
+	}
 	
+	//texto de publicacion
+	public String obtenerPublicacion(int codigoBlog, int codigoPublicacion) throws Exception {
+		revisarBlogExistente(codigoBlog);
+		Blog b = blogs.get(codigoBlog);
+		return b.obtenerPublicacion(codigoPublicacion);
+	}
+	
+	//COMENTARIO
+	
+	
+	
+	
+	//verificar el código con método parecido al de publicacion
+	private void revisarBlogExistente(int codigoBlog) throws Exception {
+		if (!blogs.containsKey(codigoBlog))
+			throw new Exception("El código de blog no es válido.");
+	}
 }
