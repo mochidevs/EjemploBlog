79a568a1ca135ba5ce487753b2c32abbec61c363
Indigo Sánchez <indigonikkis@gmail.com>
2026-04-30 01:09:24 -0600

    Add files via upload

--- EjemploBlog/src/control/Controladora.java
+++ EjemploBlog/src/control/Controladora.java
@@ -14,6 +14,12 @@ public class Controladora {
 		Blog b = new Blog(nombre, descripcion);
 		blogs.put(b.getCodigo(), b);	//añade código con su blog
 	}
+	public void borrarBlog(int codigoBlog) throws Exception {
+		if (!blogs.containsKey(codigoBlog)) 
+			throw new Exception("El código de blog no es válido");
+		blogs.remove(codigoBlog);
+	}
+	
 	public Map<Integer, String> obtenerBlogs() {
 		Map<Integer, String> resultado = new TreeMap<Integer, String>();
 		for (Blog b : blogs.values()) {
@@ -21,4 +27,12 @@ public class Controladora {
 		}
 		return resultado;
 	}
+	public void crearPublicacion(int codigoBlog, String titulo, String texto, String autor) throws Exception {
+		if (!blogs.containsKey(codigoBlog)) 
+			throw new Exception("El código de blog no es válido");
+		Blog b = blogs.get(codigoBlog);
+		b.crearPublicacion(titulo, texto, autor);
+	}
+	
+	
 }
---
+++ EjemploBlog/src/interfaz/EjemploBlog.java
@@ -0,0 +1,22 @@
+package interfaz;
+
+import control.Controladora;
+
+public class EjemploBlog {
+	
+	private static Controladora controladora;
+	
+	public static void menuBlog() {
+		
+	}
+	
+	public static void menuPublicacion() {
+		
+	}
+	
+	public static void main(String[] args) {
+		controladora = new Controladora()
+;	}
+	
+	
+}
